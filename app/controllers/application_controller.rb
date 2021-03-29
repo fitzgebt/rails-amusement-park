df7843bbfec09c502d2db958ee0bc16c4c13fa6f
fitzgebt <fitzgebt@gmail.com>
2021-03-29 08:15:09 -0700

    commit tree 9003cca0d3ff18e9d2179b67fbede06041faa57f

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :logged_in
+  helper_method :current_user
+
+  private
+
+  def logged_in
+    redirect_to '/' unless user_auth
+  end
+
+  def user_auth
+    !!current_user
+  end
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
 end
